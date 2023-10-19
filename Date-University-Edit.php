11aa45f493065ea0a868b5867dbcee2f24fd848a
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-10-19 14:14:36 +0700

    updata University edit✔️

--- Date-University-Edit.php
+++ Date-University-Edit.php
@@ -138,9 +138,9 @@
                                     <?php
                                     require_once 'Date-University-Edit-db.php';
                                     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                                        echo '<pre>';
-                                        print_r($_POST);
-                                        echo '</pre>';
+                                        // echo '<pre>';
+                                        // print_r($_POST);
+                                        // echo '</pre>';
                                     }
                                     ?>
                                 </form>
